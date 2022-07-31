92e05fd4e1caca3695ae491fc5e2daa0de8f1ff6
eltata96 <99769232+eltata96@users.noreply.github.com>
2022-07-30 21:51:07 -0300

    nuevo cors

--- src/main/java/com/portfolio/jgl/JglApplication.java
+++ src/main/java/com/portfolio/jgl/JglApplication.java
@@ -15,10 +15,26 @@ public class JglApplication {
 @Bean
 	public WebMvcConfigurer corsConfigurer() {
 		return new WebMvcConfigurer() {
+
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("**").allowedMethods("*").allowedHeaders("*");
+
+				registry.addMapping("/**").allowedOrigins("*");
 			}
 		};
 	}
-}
+	@Bean
+	public CorsFilter corsFilter() {
+		CorsConfiguration corsConfiguration = new CorsConfiguration();
+		corsConfiguration.setAllowCredentials(false);
+		corsConfiguration.setAllowedOrigins(Arrays.asList("*"));
+
+		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
+				"Accept", "Authorization", "Origin, Accept", "X-Requested-With",
+				"Access-Control-Request-Method", "Access-Control-Request-Headers"));
+		corsConfiguration.setExposedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Authorization",
+				"Access-Control-Allow-Origin", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"));
+		corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+		UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+		urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+		return new CorsFilter(urlBasedCorsConfigurationSource);	}
